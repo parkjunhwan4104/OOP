8fb6eb419cd3b48b0eac216af1462f8d54f475bc
parkjunhwan <73221778+parkjunhwan4104@users.noreply.github.com>
2021-11-22 11:42:40 +0900

    week11

---
+++ week11/11_problem2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+/*
+template <typename T>
+void swap(T& x, T& y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <>
+void swap<std::string>(std::string& x, std::string& y){
+	std::string temp = x;
+	x = y;
+	y = temp;
+
+
+};
+
+
+int main() {
+	int a = 1;
+	int b = 2;
+	float c = 3.3;
+	float d = 4.4;
+	std::string e = "World";
+	std::string f = "Hello";
+	swap(a, b);
+	swap(c, d);
+	swap(e, f);
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "c: " << c << ", d: " << d << std::endl;
+	std::cout << "e: " << e << ", f: " << f << std::endl;
+	return 0;
+}
+
+*/
---
+++ week11/11_problem3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+template<typename T>
+T min(T x, T y) {
+	return x < y ? x : y;
+}
+int main() {
+	int x = 3;
+	int y = 2;
+	double d = 2.1;
+	double e = 3.3;
+	std::cout << "min(x,y): " << min(x, y) << ", min(d,e): " << min(d, e) << ", min(d, x) : " << min<double>(d,x) << std::endl;
+		return 0;
+}
+*/
