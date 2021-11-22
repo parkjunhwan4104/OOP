fd4f5ea4d3d99a2956600400fc93337400c39792
parkjunhwan <73221778+parkjunhwan4104@users.noreply.github.com>
2021-11-22 11:40:16 +0900

    week4

---
+++ week4/4_problem2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+/*class Animal {
+public:
+	Animal() {};
+	Animal(int numberOfLeg) {
+		this->numberOfLeg = numberOfLeg;
+	}
+private:
+	int numberOfLeg;
+};
+int main() {
+	Animal a;
+	return 0;
+}*/
---
+++ week4/4_problem6.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+/*
+class Test {
+public:
+	int num1;
+	int num2;
+	Test(int num1_,int num2_) {
+		num1 = num1_;
+		num2 = num2_;
+
+	}
+
+	void print() {
+		std::cout << "num1 : " << num1 << std::endl;
+		std::cout << "num2 : " << num2 << std::endl;
+	}
+};
+int main() {
+	Test a = Test(1, 3);
+	a.print();
+}
+*/
