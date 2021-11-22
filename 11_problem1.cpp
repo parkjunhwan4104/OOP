9911a9b063cc89fd3b7ed42113174f3ad3c5793a
parkjunhwan <73221778+parkjunhwan4104@users.noreply.github.com>
2021-11-22 11:37:23 +0900

    Add files via upload

---
+++ 11_problem1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+template <typename T>
+void swap(T& x, T& y) {
+	T tmp = x;
+	x = y;
+	y = tmp;
+}
+int main() {
+	int a = 1;
+	int b = 2;
+	float c = 3.3;
+	float d = 4.4;
+	swap(a, b);
+	swap(c, d);
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "c: " << c << ", d: " << d << std::endl;
+	return 0;
+}
+
+*/
